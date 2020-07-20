764689a77b98733d24de5acf385be7f1e83ced4f
babyxong <babyxong@qq.com>
2020-07-20 17:42:57 +0800

    增加aio操作实例的学习，增加大小端转换函数

--- biglittle/test1.c
+++ biglittle/test1.c
@@ -1,15 +1,15 @@
 /* Header includes -----------------------------------------------------------*/
 #include <stdio.h>
 #include <stdint.h>
+#include <unistd.h>
 
 /* Macro definitions ---------------------------------------------------------*/
 /* Type definitions ----------------------------------------------------------*/
 /* Variable declarations -----------------------------------------------------*/
 /* Variable definitions ------------------------------------------------------*/
 /* Function declarations -----------------------------------------------------*/
-static void BigAndLittleEndianConversion(void *data, uint32_t size);
-
 /* Function definitions ------------------------------------------------------*/
+
 
 /**
   * @brief  Big and little endian conversion.
@@ -17,17 +17,21 @@ static void BigAndLittleEndianConversion(void *data, uint32_t size);
   * @param  [in] size: The size of the data to be converted.
   * @return None.
   */
-static void BigAndLittleEndianConversion(void *data, uint32_t size)
+static inline void BigLittleSwap( uint8_t *pdata, uint32_t size)
 {
-  for(uint32_t i = 1; i < size; i++)
-  {
-    for(uint32_t j = 1; j < size - i + 1; j++)
-    {
-      uint8_t c = *(((uint8_t *)data) + j - 1);
-      *(((uint8_t *)data) + j - 1) = *(((uint8_t *)data) + j);
-      *(((uint8_t *)data) + j) = c;
-    }
-  }
+	uint32_t i;
+	uint32_t j;
+	uint8_t  tmp;
+ 
+	for (i = 0; i < (size+1)/2; i++)
+	{
+		tmp = *(pdata+i);
+		*(pdata+i) = *(pdata+size-i-1);
+		*(pdata+size-i-1) = tmp;
+	}
+ 
+	printf(" pdata: %d \n", *pdata);
+  
 }
 
 /**
@@ -37,14 +41,29 @@ static void BigAndLittleEndianConversion(void *data, uint32_t size)
   */
 int main(void)
 {
-  
-  uint64_t bigEndianData             = 0x0123456789ABCDEF;
-  
-	printf(" bigEndianData size: %u  \n", sizeof(bigEndianData));
-  
-  BigAndLittleEndianConversion(&bigEndianData, sizeof(bigEndianData));
-  
+	uint32_t n =0;
 
-	printf("bigEndianData = %#lx\n", bigEndianData);
+	uint8_t Data1[]={0x01, 0x02, 0x03, 0x04, 0x05};  
+ 
+	BigLittleSwap(&Data1[0], sizeof(Data1));
+
+	for (n =0 ;n< sizeof(Data1);n++)
+	{
+		printf("swap1 %d = %#lx\n", n,*(Data1+n));
+	}
+	
+	BigLittleSwap(&Data1[0], sizeof(Data1));
+	for (n =0 ;n< sizeof(Data1);n++)
+	{
+		printf("swap2 %d = %#lx\n", n,*(Data1+n));
+	}
+	
+	uint64_t udata = 0x0123456789ABCDEF;
+	
+	BigLittleSwap((uint8_t *)&udata, sizeof(udata));
+	printf("sizeof : %u \n", sizeof(udata));
+	printf("bigEndianData %#lx \n ", udata);
+	
+	
 
 }
