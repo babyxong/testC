31d126d37975968e9be31c5d219c6dbe90985af4
babyxong <babyxong@qq.com>
2021-04-01 14:31:23 +0800

    update

--- file/test.c
+++ file/test.c
@@ -11,25 +11,22 @@ client.c
 #include <arpa/inet.h>
 #define PORT 8888
  
- #define STRING  "Hello World!!!!  mem ok"
- 
- typedef struct AAA{
-     int AAA
-     void *pA;;
-     
- }stAAA;
- 
+#define ROUND_UP_INI(x , a)  (((x) % (a)) ? ((x) / (a) +1):((x)/(a)))
+#define ROUND_UP(x , a)  ( ((x)+(a-1)) & ~(a -1))
  
  
 int main()
 {
-    char * pchar;
+    printf("=*1===%d \n", ROUND_UP(0,4));
+    printf("=*1===%d \n", ROUND_UP(3,4));
+    printf("=*1===%d \n", ROUND_UP(13,4));
     
-    FILE *p = fopen();
-    
-    
-    
-    
-    
+    printf("=1===%d \n", ROUND_UP_INI(0,4));
+     printf("=1===%d \n", ROUND_UP_INI(3,4));
+     printf("=1===%d \n", ROUND_UP_INI(4,4));
+    printf("=1===%d \n", ROUND_UP_INI(13,4));
+    printf("=2===%d \n", ROUND_UP_INI(14,4));
+    printf("=3===%d \n", ROUND_UP_INI(15,4));
+    printf("=4===%d \n", ROUND_UP_INI(16,4));
     
 }
